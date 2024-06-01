39ef7e7eba01e1edd0d304a67fe6351fdc1b296e
Atsushi SHIBATA <shibata-atsushi@aiit.ac.jp>
2024-06-01 09:09:11 +0000

    github action check

--- script.sh
+++ script.sh
@@ -1,3 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "hello"
+if [ $# -ne 2 ]; then          # 引数の数が2でない場合(not equal)
+  echo "input 2 argments" 1>&2 # エラーメッセージ
+  exit 1                       # 終了ステータス
+fi
+expr $1 + $2 > /dev/null 2>&1  # exprに数字以外を渡すと終了ステータスが2以上になる
+if [ $? -ge 2 ]; then          # その場合
+  echo "input natural number" 1>&2  # エラーメッセージ
+  exit 1                       # 終了ステータス
+fi
+
+if [ $1 -eq $2 ]; then         # 一致判定
+    echo "$1 is equal to $2"   # 一致してる場合のメッセージ		
+else
+    echo "$1 is not equal to $2"  # 一致していない場合のメッセージ
+fi
--- test_script.sh
+++ test_script.sh
@@ -1,12 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
-result=$(./script.sh)
+tmp=/tmp/$$					# 変数を使って表記を短く
+echo "input 2 argments" > $tmp-args		# 回答準備：引数の数
+echo "input natural number" > $tmp-nat 	# 回答準備：数字じゃない
 
-if [ $result = "hello" ];
-then
-	echo "OK"
-	exit 0
-else
-	echo "NG" 1>&2
-	exit 1
-fi
+ERROR_EXIT () {
+  echo "$1" >&2		# エラーメッセージ(引数1)を標準エラー出力に表示
+  rm -f $tmp-*			# 作ったファイルの削除
+  exit 1			# エラー終了
+}
+
+# テスト開始
+# teat1: 引数の数が足りない
+./script.sh 2> $tmp-ans && ERROR_EXIT "error in test1-1"	# エラーメッセージを関数に渡す
+diff $tmp-ans $tmp-args || ERROR_EXIT "error in test1-2"
